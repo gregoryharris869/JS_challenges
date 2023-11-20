4c3e4d4ca715dc3157e9af61177bd1658d14fa70
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-11-20 16:26:43 -0600

    updated challenge

--- traversy-js-challenges/01-basic-challenges-1/03-calculator/calculator.js
+++ traversy-js-challenges/01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,22 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  // Declare a variable to store the result
+  let result;
+
+  // Use if/else if/else statements to determine which operation to perform
+  if (operator === "+") {
+    result = num1 + num2;
+  } else if (operator === "-") {
+    result = num1 - num2;
+  } else if (operator === "*") {
+    result = num1 * num2;
+  } else if (operator === "/") {
+    result = num1 / num2;
+  } else {
+    // If the operator is not one of the above, throw an error
+    throw new Error("Invalid operator");
+  }
+
+  return result;
+}
 
 module.exports = calculator;
